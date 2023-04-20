83245b9c5ce9695c3a9010133d6b54c6458fcbeb
Fitz Gerald <gfitzsicat@gmail,com>
2023-04-20 12:19:01 -0400

    test 2

--- joke.js
+++ joke.js
@@ -1,6 +1,9 @@
 const jokeContainer = document.getElementById("joke");
 const btn = document.getElementById('btn');
+const badBtn = document.getElementById('badBtn');
 const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+const badUrl = "https://v2.jokeapi.dev/joke/Dark,Spooky?type=single"
+
 
 
 let getJoke = () => {
@@ -13,5 +16,20 @@ let getJoke = () => {
     });
 }
 btn.addEventListener("click", getJoke);
-getJoke();
+getJoke;
 
+
+
+let getBadJoke = () => { 
+    jokeContainer.classList.remove("fade");
+    fetch(badUrl)
+    .then(data => data.json())
+    .then(item => {
+        jokeContainer.textContent = `${item.joke}`;
+        jokeContainer.classList.add("fade")
+    })
+}
+badBtn.addEventListener('click', () =>{
+    alert("These are BAD JOKES and OFFENSIVE!. You may LEAVE NOW otherwise click 'ok' or press 'enter'");
+    getBadJoke()
+})
